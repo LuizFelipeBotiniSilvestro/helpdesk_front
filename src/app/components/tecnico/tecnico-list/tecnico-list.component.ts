3f0c0671a70ff20b60fe12e292cb8bf93097f9c5
LuizFelipeBotiniSilvestro <luizfelipebssideropolis@gmail.com>
2023-05-20 15:30:34 -0300

    Criado componente de login

--- src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
+++ src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Tecnico } from 'src/app/models/tecnico';
 
 @Component({
   selector: 'app-tecnico-list',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TecnicoListComponent implements OnInit {
 
+    
   constructor() { }
+  
+  ELEMENT_DATA: Tecnico[] = [
+    {
+      id: 1,
+      nome: 'Luiz Felipe',
+      cpf: '117.655.499-90',
+      email: 'email@email.com',
+      senha: '1234',
+      perfis: ['0'],
+      dataCriacao: '18/05/2023'
+    }
+  ];
+
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol', "acoes"];
+  dataSource = new MatTableDataSource<Tecnico>(this.ELEMENT_DATA); // this. "Fazendo referência"
 
   ngOnInit(): void {
+  }
+
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
   }
 
 }
