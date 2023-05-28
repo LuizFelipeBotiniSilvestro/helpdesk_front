85691971bc3f4f5e646409ab6101d0a6c9459bd1
LuizFelipeBotiniSilvestro <luizfelipebssideropolis@gmail.com>
2023-05-28 00:10:15 -0300

    Rota para componente chamado-read criada

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -20,7 +20,7 @@ export class NavComponent implements OnInit {
 
   // Quando iniciar o sistema vai para página home.
   ngOnInit(): void {
-    this.router.navigate(['chamados/update/1'])
+    this.router.navigate(['chamados/read/1'])
   }
 
   // Função de logout no sistema.
